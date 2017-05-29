62121171fd9574d30e95db26473aaa54463cbdb5
Michal Juralowicz <michal.juralowicz@outlook.com>
2017-05-29 10:22:38 +0200

    git fix 1

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import * as express from "express";
+import * as handlebars from "express-handlebars";
 
 let app:express.Application = express();
 
 
 import {indexRouter} from "./routers/index";
 import {router as productsRouter} from "./routers/products";
+
+app.set("view engine", "hbs");
 
 app.use("/", indexRouter);
 app.use("/products", productsRouter);
