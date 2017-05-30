b337a32f4f6cdd7a2aedde0b841e894b16e6c1aa
Michal Juralowicz <michal.juralowicz@outlook.com>
2017-05-30 11:32:12 +0200

    fix

--- dist/routers/index.js
+++ dist/routers/index.js
@@ -1,15 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//var express = require("express");
 var express = require("express");
-//const pool = require("./../../bin/db.js");
 var indexRouter = express.Router();
 exports.indexRouter = indexRouter;
 indexRouter.get("/", function (req, res) {
     console.log("Got a index request :3");
     res.status(200).render("home", { title: "Home | Main" });
-    // ZAPYTANIE DO BAZY //
-    // pool.query("SELECT * FROM users;",(err: any,res: any)=>{
-    //     console.log(res);
-    // });
 });
