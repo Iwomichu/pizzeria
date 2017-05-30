d2bec5d015dc523941f1df8e9f53d36086b4b3d8
Michal Juralowicz <michal.juralowicz@outlook.com>
2017-05-30 14:45:27 +0200

    models - userv1

---
+++ src/database/models/User.ts
@@ -0,0 +1,15 @@
+export class User{
+    id: number;
+    login: string;
+    password: string;
+    permissions: number;
+    email: string;
+    // additionalInfo: boolean;
+    // name: string;
+    // lastname: string;
+    // city: string;
+    // street: string;
+    // address: number;
+    // flat: number;
+    // phone: string;
+}
