960005aad6648f18e479141dcf48891995c7bbbe
pampi848 <pampi.com@gmail.com>
2017-06-01 00:29:26 +0200

    Git ignore, - zbędny komentarz

--- src/db.ts
+++ src/db.ts
@@ -1,47 +1,5 @@
 import { MongoClient } from "mongodb";
 
-// export class DatabaseCommunication {
-//     public static methodError = function (err :Error, result :object,db :any, callback :Function) {
-//         if(err)return console.error(err);
-//         callback(result);
-//         db.close();
-//     }
-
-//     public static connect = function (collection: string, document: object){
-        
-//                console.log("0", document);
-//            MongoClient.connect("mongodb://localhost:27017/pizzeria").then( (db) =>{
-//                console.log("1", document);
-//                DatabaseCommunication.insertOne
-//             // if (err) throw err;
-//             // console.log("hello from the databaseee");
-//             // switch (methodName) {
-//             //     case "insertOne":
-//             //     console.log(document);
-//             //         db.collection(collection).insertOne(document, (err, result)=>{DatabaseCommunication.methodError(err, result, db, callback)}).then(db.close);
-//             //         break;
-//             // }
-            
-//         }).then( (db) =>{DatabaseCommunication.dbClose}).catch(console.log);
-//     }
-//     public static insertOne = function(db :any, err: Error, collection: string, document: object): Promise<string> {
-//             return new Promise<string>(
-//                 (resolve,reject) => {
-//                console.log("2", document);
-//                     resolve(db.collection(collection).insertOne(document));
-//                     reject(console.error(err));
-//                 }
-//             );
-//     }
-//      public static dbClose = function(db :any): Promise<string> {
-//             return new Promise<string>(
-//                 resolve => {
-//                     resolve(db.close());
-//                 }
-//             );
-//     }
-    
-// }
 export class DatabaseCommunication {
     
     public static connect = function(): Promise<DatabaseCommunication>{
