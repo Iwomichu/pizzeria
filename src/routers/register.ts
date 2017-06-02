851e0f469c5290cf1ad3c3b9c5e5d1f699d303fe
Michał <pampi.com@gmail.com>
2017-06-02 09:38:56 +0200

    rename db mongo file

--- src/routers/register.ts
+++ src/routers/register.ts
@@ -28,4 +28,27 @@ router.get("/success/:name?/:lastname?", (req:express.Request, res:express.Respo
     res.send("Rejestracja udana");
 });
 
+
+
+
+router.post("/pg", (req:express.Request, res:express.Response, next:express.NextFunction)=>{
+    console.log("Accessing register page");
+    
+   DatabaseCommunication.connect()
+        .then((db) => {DatabaseCommunication.insertOne(db, "users", req.body, false, (result: any)=>{
+                            DatabaseCommunication.findOne(db, "users", {login: result.ops[0].login} , true, (result: any)=>{  
+                                console.log(result);   
+                                res.redirect("/register/success/"+result.name+"/"+result.lastname);
+                            })
+                        })
+    }).catch((err) => {console.error(err)});
+});
+
+router.get("/success/:name?/:lastname?", (req:express.Request, res:express.Response, next:express.NextFunction)=>{
+    if(req.params.name && req.params.lastname)console.log("%s %s successfuly registed", req.params.name, req.params.lastname);
+    else console.log("Somebody successfuly registed");
+    res.send("Rejestracja udana");
+});
+
+
 export {router};
